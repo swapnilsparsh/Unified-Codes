cd429733136558be1613d06e0af4f135daf48fc9
jatiinyadav <jatin27yadav@gmail.com>
2020-10-28 22:34:09 +0530

    name.py

---
+++ name.py
@@ -0,0 +1,14 @@
+
+name = 'youtube'
+
+print(r"Myname's " + name)
+print(name[0])
+print(name[-3])
+print(name[0:5])
+print(name[:5])
+name = 'HelloWorld'
+print(name[6])
+print(name[2:7])
+print(name[6])
+print('Hii'+ name[0:6])
+print('Hello' + name[:10])
